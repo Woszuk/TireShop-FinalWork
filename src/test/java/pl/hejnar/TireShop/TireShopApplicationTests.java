04f63a63c1ccc7c03260284779759f1747fdfc6e
Woszuk <Woszuk@users.noreply.github.com>
2021-02-05 18:54:27 +0100

    Program structure initialization

---
+++ src/test/java/pl/hejnar/TireShop/TireShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hejnar.TireShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
