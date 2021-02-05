96673866a71fca1b0d14af476205853df2f69315
Woszuk <Woszuk@users.noreply.github.com>
2021-02-05 20:04:14 +0100

    added fragemnts for login and registration

--- src/main/resources/static/js/account-navigation.js
+++ src/main/resources/static/js/account-navigation.js
@@ -1,6 +1,6 @@
 (() => {
-    let account = document.querySelectorAll(".main-navigation__account-item--link.sign-in");
-    let accountMobile = document.querySelectorAll(".main-navigation__mobile-account .main-navigation__account-item--link.sign-in");
+    let account = document.querySelectorAll(".main-navigation__account-item--link");
+    let accountMobile = document.querySelectorAll(".main-navigation__mobile-account .main-navigation__account-item--link");
     let signIn = document.querySelector(".banner__sign-in");
     let closeSignIn = document.querySelector(".banner__sign-in--close");
     let closeRegister = document.querySelector(".banner__register--close");
@@ -34,24 +34,15 @@
         register.classList.toggle("banner__sign-in--display")
     }
 
-    if(accountMobile.length > 0){
+    if(accountMobile!= null){
         accountMobile[0].addEventListener("click", toggleSignIn);
         accountMobile[1].addEventListener("click", toggleRegister)
     }
 
-    if(account.length > 0){
-        account[0].addEventListener("click", toggleSignIn);
-        account[1].addEventListener("click", toggleRegister);
-    }
-
-    if(closeSignIn != null){
-        closeSignIn.addEventListener("click", toggleSignIn);
-
-    }
-
-    if(closeRegister != null){
-        closeRegister.addEventListener("click", toggleRegister);
-        haveAccount.addEventListener("click", toggleSignReg);
-        newAccount.addEventListener("click", toggleSignReg);
-    }
+    account[0].addEventListener("click", toggleSignIn);
+    account[1].addEventListener("click", toggleRegister)
+    closeSignIn.addEventListener("click", toggleSignIn);
+    closeRegister.addEventListener("click", toggleRegister);
+    newAccount.addEventListener("click", toggleSignReg);
+    haveAccount.addEventListener("click", toggleSignReg);
 })();
