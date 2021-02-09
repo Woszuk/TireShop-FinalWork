1e856f874989bccf7286f4b77055510631f8dc27
Woszuk <Woszuk@users.noreply.github.com>
2021-02-09 19:07:37 +0100

    configuration Spring Security

---
+++ src/main/java/pl/hejnar/tireshop/auth/ApplicationUserService.java
@@ -0,0 +1,41 @@
+package pl.hejnar.tireshop.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.hejnar.tireshop.entity.Role;
+import pl.hejnar.tireshop.entity.User;
+
+import pl.hejnar.tireshop.service.UserServiceImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ApplicationUserService implements UserDetailsService {
+
+    private UserServiceImpl userService;
+
+    @Autowired
+    public void setUserRepository(UserServiceImpl userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException(String.format("Username %s not found", username));
+        }
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        for(Role role: user.getRoles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
---
+++ src/main/java/pl/hejnar/tireshop/configure/SecurityConfig.java
@@ -0,0 +1,34 @@
+package pl.hejnar.tireshop.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.hejnar.tireshop.auth.ApplicationUserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationUserService customUserDetailsService() {
+        return new ApplicationUserService();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().disable();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().formLogin().loginPage("/signin").defaultSuccessUrl("/somePage")
+                .and().logout();
+    }
+}
